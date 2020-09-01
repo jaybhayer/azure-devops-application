87ecaa26c9f7d2ce91806d7c4006c35b9f4464f5
Rajnikant_Jaybhaye <45669849+jaybhayer@users.noreply.github.com>
2020-09-01 19:23:59 +0530

    Update protractor.conf.js

--- src/angular7/e2e/protractor.conf.js
+++ src/angular7/e2e/protractor.conf.js
@@ -1,30 +1,30 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 const { JUnitXmlReporter } = require('jasmine-reporters');
 
-//process.env.CHROME_BIN = process.env.CHROME_BIN || require("puppeteer").executablePath();
+process.env.CHROME_BIN = process.env.CHROME_BIN || require("puppeteer").executablePath();
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
-  multiCapabilities: {
+ /* multiCapabilities: {
     'browserName': 'firefox',
       firefoxOptions: {
       args: ['--headless']
     },
       'moz:firefoxOptions': {
       args: [ '--headless' ]
-    }
-    /*'browserName': 'chrome',
+    }*/
+    'browserName': 'chrome',
 
     chromeOptions: {
-      args: ["--headless", "--disable-gpu", "--window-size=1200,900"],
+      args: ["--no-sandbox", "--headless", "--disable-gpu", "--window-size=1200,900"],
       binary: process.env.CHROME_BIN
-    }*/
+    }
   },
     //chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_77.0.3865.75',
-  geckoDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/geckodriver-v0.27.0',
+  //geckoDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/geckodriver-v0.27.0',
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
